99e46906e8d196caf4219d8699e752abe055c609
Ganavi S S <149884537+Ganavisuresh@users.noreply.github.com>
2024-05-14 22:14:11 +0530

    Add files via upload

---
+++ URL-Shortener/urlshortner-backend-main/backend/routes/genUrl.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const validUrl = require("valid-url");
+const shortid = require("shortid");
+const dotenv = require("dotenv").config();
+const Url = require("../models/Url");
+
+router.post("/", async (request, response) => {
+  const { longUrl } = request.body;
+  const baseUrl = process.env.BASEURI;
+
+  const urlCode = shortid.generate();
+  if (validUrl.isUri(longUrl)) {
+    try {
+      let url = await Url.findOne({ longUrl });
+      if (url) {
+        response.json(url);
+      } else {
+        const shortUrl = baseUrl + "/" + urlCode;
+        url = new Url({
+          urlCode,
+          longUrl,
+          shortUrl,
+          date: new Date(),
+        });
+
+        await url.save();
+        response.json(url);
+      }
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).json("Server Error");
+    }
+  } else {
+    response.status(401).json("Invalid Long URL");
+  }
+});
+
+module.exports = router;
